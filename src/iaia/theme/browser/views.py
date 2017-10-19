5db299f13cf46e8fb55ed2f2c78eeebe87e3cd4e
henry <ah13441673@gmail.com>
2017-10-19 16:08:48 +0800

    demo done

---
+++ src/iaia/theme/browser/views.py
@@ -0,0 +1,7 @@
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api
+
+
+class IaiaMacro(BrowserView):
+    """ """
