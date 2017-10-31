5aab61cf47eca58c16531bd0946447baf687c99c
henry <ah13441673@gmail.com>
2017-10-31 11:40:00 +0800

    1031-1

--- src/iaia/theme/browser/views.py
+++ src/iaia/theme/browser/views.py
@@ -5,3 +5,21 @@ from plone import api
 
 class IaiaMacro(BrowserView):
     """ """
+
+
+class Send_mail(BrowserView):
+    template = ViewPageTemplateFile('template/send_mail.pt')
+
+    def __call__(self):
+        email=self.request.form['email']
+        message=self.request.form['message']
+
+        api.portal.send_email(
+            recipient="henry@mingtak.com.tw",
+            sender=email,
+            subject="Trappist",
+            body=message,
+        )
+        return  
+
+        
--- src/iaia/theme/theme/js/custom.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    alert('s')
-    if($('body').is('.template-listing_view')){
-        $('article').addClass('work');
-    }
-});
